d28a1cab6a9194bc1f8aa6196cb2826d70b4a6e7
v3aqb <sgzz.cj@gmail.com>
2014-12-31 12:46:06 +0800

    hi

---
+++ encrypt.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2012 clowwindy
+# Copyright (c) 2013 - 2014 v3aqb
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import hashlib
+import string
+import struct
+import logging
+from collections import defaultdict, deque
+from repoze.lru import lru_cache
+from ctypes_libsodium import Salsa20Crypto
+try:
+    from M2Crypto.EVP import Cipher
+    import M2Crypto.Rand
+    random_string = M2Crypto.Rand.rand_bytes
+except ImportError:
+    random_string = os.urandom
+    try:
+        from streamcipher import StreamCipher as Cipher
+    except ImportError:
+        Cipher = None
+
+
+def get_table(key):
+    m = hashlib.md5()
+    m.update(key)
+    s = m.digest()
+    (a, b) = struct.unpack('<QQ', s)
+    table = [c for c in string.maketrans('', '')]
+    for i in range(1, 1024):
+        table.sort(lambda x, y: int(a % (ord(x) + i) - a % (ord(y) + i)))
+    return table
+
+
+def check(key, method):
+    if method.lower() == 'table':
+        encrypt_table = ''.join(get_table(key))
+        string.maketrans(encrypt_table, string.maketrans('', ''))
+    else:
+        try:
+            Encryptor(key, method)  # test if the settings if OK
+        except Exception as e:
+            logging.error(e)
+            raise e
+
+
+@lru_cache(128)
+def init_table(key):
+    encrypt_table = ''.join(get_table(key))
+    decrypt_table = string.maketrans(encrypt_table, string.maketrans('', ''))
+    return (encrypt_table, decrypt_table)
+
+
+@lru_cache(128)
+def EVP_BytesToKey(password, key_len, iv_len):
+    # equivalent to OpenSSL's EVP_BytesToKey() with count 1
+    # so that we make the same key and iv as nodejs version
+    m = []
+    i = 0
+    while len(b''.join(m)) < (key_len + iv_len):
+        md5 = hashlib.md5()
+        data = password
+        if i > 0:
+            data = m[i - 1] + password
+        md5.update(data)
+        m.append(md5.digest())
+        i += 1
+    ms = b''.join(m)
+    key = ms[:key_len]
+    iv = ms[key_len:key_len + iv_len]
+    return (key, iv)
+
+
+class sized_deque(deque):
+    def __init__(self):
+        deque.__init__(self, maxlen=1048576)
+
+method_supported = {
+    'aes-128-cfb': (16, 16),
+    'aes-192-cfb': (24, 16),
+    'aes-256-cfb': (32, 16),
+    'aes-128-ofb': (16, 16),
+    'aes-192-ofb': (24, 16),
+    'aes-256-ofb': (32, 16),
+    'aes-128-ctr': (16, 16),
+    'aes-192-ctr': (24, 16),
+    'aes-256-ctr': (32, 16),
+    'camellia-128-cfb': (16, 16),
+    'camellia-192-cfb': (24, 16),
+    'camellia-256-cfb': (32, 16),
+    'camellia-128-ofb': (16, 16),
+    'camellia-192-ofb': (24, 16),
+    'camellia-256-ofb': (32, 16),
+    'camellia-128-ctr': (16, 16),
+    'camellia-192-ctr': (24, 16),
+    'camellia-256-ctr': (32, 16),
+    'cast5-cfb': (16, 8),
+    'cast5-ofb': (16, 8),
+    'cast5-ctr': (16, 8),
+    'seed-cfb': (16, 16),
+    'seed-ofb': (16, 16),
+    'seed-ctr': (16, 16),
+    'rc4': (16, 0),
+    'rc4-md5': (16, 16),
+    'salsa20': (32, 8),
+    'chacha20': (32, 8),
+}
+
+USED_IV = defaultdict(sized_deque)
+
+
+def create_rc4_md5(method, key, iv, op):
+    md5 = hashlib.md5()
+    md5.update(key)
+    md5.update(iv)
+    rc4_key = md5.digest()
+    return Cipher('rc4', rc4_key, '', op)
+
+
+class Encryptor(object):
+    def __init__(self, key, method=None, servermode=False):
+        if method == 'table':
+            method = None
+        self.key = key
+        self.method = method
+        self.servermode = servermode
+        self.iv = None
+        self.iv_sent = False
+        self.cipher_iv = ''
+        self.decipher = None
+        if method is not None:
+            self.cipher = self.get_cipher(key, method, 1, random_string(32))
+        else:
+            self.cipher = None
+            self.decipher = 0
+            self.encrypt_table, self.decrypt_table = init_table(key)
+
+    def get_cipher_len(self, method):
+        method = method.lower()
+        m = method_supported.get(method, None)
+        return m
+
+    def iv_len(self):
+        return len(self.cipher_iv)
+
+    def get_cipher(self, password, method, op, iv):
+        password = password.encode('utf-8')
+        method = method.lower()
+        m = self.get_cipher_len(method)
+        if m:
+            key, _ = EVP_BytesToKey(password, m[0], 0)
+            iv = iv[:m[1]]
+            if op == 1:
+                self.cipher_iv = iv  # this iv is for cipher, not decipher
+            if method == 'rc4-md5':
+                return create_rc4_md5(method, key, iv, op)
+            elif method in ('salsa20', 'chacha20'):
+                return Salsa20Crypto(method, key, iv, op)
+            else:
+                return Cipher(method.replace('-', '_'), key, iv, op)
+        raise ValueError('method %s not supported' % method)
+
+    def encrypt(self, buf):
+        if len(buf) == 0:
+            return buf
+        if self.method is None:
+            return string.translate(buf, self.encrypt_table)
+        else:
+            if self.iv_sent:
+                return self.cipher.update(buf)
+            else:
+                self.iv_sent = True
+                return self.cipher_iv + self.cipher.update(buf)
+
+    def decrypt(self, buf):
+        if len(buf) == 0:
+            return buf
+        if self.method is None:
+            return string.translate(buf, self.decrypt_table)
+        else:
+            if self.decipher is None:
+                decipher_iv_len = self.get_cipher_len(self.method)[1]
+                decipher_iv = buf[:decipher_iv_len]
+                if self.servermode:
+                    if decipher_iv in USED_IV[self.key]:
+                        raise ValueError('iv reused, possible replay attrack')
+                    USED_IV[self.key].append(decipher_iv)
+                self.decipher = self.get_cipher(self.key, self.method, 0, decipher_iv)
+                buf = buf[decipher_iv_len:]
+                if len(buf) == 0:
+                    return buf
+            return self.decipher.update(buf)
---
+++ hxsserver.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 clowwindy
+# Copyright (c) 2014 v3aqb
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import with_statement
+
+
+__version__ = '1.0.0'
+
+import sys
+try:
+    import gevent
+    import gevent.monkey
+    gevent.monkey.patch_all(dns=gevent.version_info[0] >= 1)
+except ImportError:
+    gevent = None
+    print >>sys.stderr, 'warning: gevent not found, using threading instead'
+
+import errno
+import socket
+import thread
+import threading
+import time
+import random
+import SocketServer
+import struct
+import binascii
+import hashlib
+import logging
+import encrypt
+import os
+import urlparse
+from collections import defaultdict, deque
+from util import create_connection, parse_hostport
+from dh import DH
+
+users = {'user': 'pass'}
+
+
+def hex2bytes(data):
+    data = '0' * (len(data) % 2) + data
+    return binascii.unhexlify(data)
+
+
+def bytes2hex(data):
+    return binascii.hexlify(data).decode()
+
+
+class KeyManager:
+    userpkeys = defaultdict(deque)
+    pkeyuser = {}
+    pkeykey = {}
+    pkeytime = {}
+
+    @classmethod
+    def create_key(cls, user, client_pkey):
+        valid = 1
+        if not valid:
+            return 0, 0
+        if len(cls.userpkeys[user]) > 3:
+            cls.del_key(cls.userpkeys[user][0])
+        dh = DH()
+        shared_secret = dh.genKey(bytes2hex(client_pkey))
+        client_pkey = hashlib.md5(client_pkey).digest()
+        cls.userpkeys[user].append(client_pkey)
+        cls.pkeyuser[client_pkey] = user
+        cls.pkeykey[client_pkey] = shared_secret
+        cls.pkeytime[client_pkey] = time.time()
+        return hex2bytes(dh.hexPub), users[user]
+
+    @classmethod
+    def check_key(cls, pubk):
+        if pubk not in cls.pkeykey:
+            return 1
+        if cls.pkeytime[pubk] < time.time() - 6 * 3600:
+            cls.del_key(pubk)
+            return 1
+
+    @classmethod
+    def del_key(cls, pkey):
+        user = cls.pkeyuser[pkey]
+        del cls.pkeyuser[pkey]
+        del cls.pkeytime[pkey]
+        del cls.pkeykey[pkey]
+        cls.userpkeys[user].remove(pkey)
+
+
+class HXSocksServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    allow_reuse_address = True
+
+    def __init__(self, serverinfo, RequestHandlerClass, bind_and_activate=True):
+        self.serverinfo = serverinfo
+        p = urlparse.urlparse(serverinfo)
+        self.PSK = urlparse.parse_qs(p.query).get('PSK', [''])[0]
+        reverse = urlparse.parse_qs(p.query).get('reverse', [''])[0]
+        self.reverse = parse_hostport(reverse) if reverse else None
+        addrs = socket.getaddrinfo(p.hostname, p.port)
+        if not addrs:
+            raise ValueError('cant resolve listen address')
+        self.address_family = addrs[0][0]
+        server_address = (p.hostname, p.port)
+        SocketServer.TCPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate=bind_and_activate)
+
+    def server_activate(self):
+        self.socket.listen(self.request_queue_size)
+
+    def get_request(self):
+        connection = self.socket.accept()
+        connection[0].settimeout(10)
+        return connection
+
+
+class HXSocksHandler(SocketServer.StreamRequestHandler):
+    timeout = 10
+    bufsize = 8192
+
+    def handle(self):
+        close = 0
+        while not close:
+            cipher = encrypt.Encryptor(self.server.PSK, 'chacha20', servermode=True)
+            cmd = ord(cipher.decrypt(self.rfile.read(9)))
+            if cmd == 0:  # client key exchange
+                ts = cipher.decrypt(self.rfile.read(4))
+                if abs(struct.unpack('>I', ts)[0] - time.time()) > 600:
+                    logging.error('bad timestamp, possible replay attrack')
+                    return
+                pklen = struct.unpack('>H', cipher.decrypt(self.rfile.read(2)))[0]
+                client_pkey = cipher.decrypt(self.rfile.read(pklen))
+                client_auth = cipher.decrypt(self.rfile.read(32))
+                for user, passwd in users.items():
+                    if hashlib.sha256(client_pkey + user.encode() + passwd.encode()).digest() == client_auth:
+                        client = user
+                        break
+                else:
+                    return logging.error('user not found, close socket')
+                logging.info('client: %s is asking for a new key' % user)
+                pkey, passwd = KeyManager.create_key(client, client_pkey)
+                if pkey:
+                    data = chr(0) + struct.pack('>H', len(pkey)) + pkey + hashlib.sha256(client_pkey + pkey + user.encode() + passwd.encode()).digest()
+                    self.wfile.write(cipher.encrypt(data))
+                else:
+                    logging.error('client: %s create new key failed!' % user)
+                    return
+            elif cmd == 1:  # a connect request
+                client_pkey = cipher.decrypt(self.rfile.read(16))
+                if KeyManager.check_key(client_pkey):
+                    rint = random.randint(64, 255)
+                    self.wfile.write(cipher.encrypt(chr(1) + chr(rint)) + os.urandom(rint))
+                    continue
+                cipher = encrypt.Encryptor(KeyManager.pkeykey[client_pkey], 'chacha20', servermode=True)
+                ts = cipher.decrypt(self.rfile.read(12))
+                if abs(struct.unpack('>I', ts)[0] - time.time()) > 600:
+                    logging.error('bad timestamp, possible replay attrack')
+                    return
+                host_len = ord(cipher.decrypt(self.rfile.read(1)))
+                hostport = cipher.decrypt(self.rfile.read(host_len))
+                logging.info('CONNECT %s' % hostport)
+                addr, port = parse_hostport(hostport)
+                self.wfile.write(cipher.encrypt(chr(0)))
+                try:
+                    remote = None
+                    logging.info('server %s:%d request %s:%d from %s:%d' % (self.server.server_address[0], self.server.server_address[1],
+                                 addr, port, self.client_address[0], self.client_address[1]))
+                    data = cipher.decrypt(self.connection.recv(self.bufsize))
+                    if self.server.reverse:
+                        remote = create_connection(self.server.reverse, timeout=1)
+                        if data.startswith((b'GET', b'POST', b'HEAD', b'PUT', b'DELETE', b'TRACE', b'OPTIONS', b'PATCH', b'CONNECT')) and b'HTTP/1' in data and b'\r\n' in data:
+                            data = data.replace(b'\r\n', ('\r\nss-realip: %s:%s\r\nss-client: %s\r\n' % (self.client_address[0], self.client_address[1], self.server.key)).encode('latin1'), 1)
+                        else:
+                            a = 'CONNECT %s:%d HTTP/1.0\r\nss-realip: %s:%s\r\nss-client: %s\r\n\r\n' % (addr, port, self.client_address[0], self.client_address[1], self.server.key)
+                            remote.sendall(a.encode('latin1'))
+                            remoterfile = remote.makefile('rb', 0)
+                            d = remoterfile.readline()
+                            while d not in (b'\r\n', b'\n', b'\r'):
+                                if not d:
+                                    raise IOError(0, 'remote closed')
+                                d = remoterfile.readline()
+                    if not remote:
+                        remote = create_connection((addr, port), timeout=10)
+                    remote.sendall(data)
+                    # self.remote.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+                except (IOError, OSError) as e:  # Connection refused
+                    logging.warn('server %s:%d %r on connecting %s:%d' % (self.server.server_address[0], self.server.server_address[1], e, addr, port))
+                    return
+                self.forward_tcp(self.connection, remote, cipher, timeout=60)
+                return
+            else:
+                close = 1
+
+    def forward_tcp(self, local, remote, cipher, timeout=60):
+        def _io_copy(dest, source, timeout, cipher):
+            try:
+                dest.settimeout(timeout)
+                source.settimeout(timeout)
+                while 1:
+                    data = source.recv(self.bufsize)
+                    if not data:
+                        break
+                    dest.sendall(cipher(data))
+            except socket.timeout:
+                pass
+            except (IOError, OSError) as e:
+                if e.args[0] not in (errno.ECONNABORTED, errno.ECONNRESET, errno.ENOTCONN, errno.EPIPE):
+                    raise
+                if e.args[0] in (errno.EBADF,):
+                    return
+            finally:
+                for sock in (dest, source):
+                    try:
+                        sock.close()
+                    except (IOError, OSError):
+                        pass
+        thread.start_new_thread(_io_copy, (remote.dup(), local.dup(), timeout, cipher.decrypt))
+        _io_copy(local, remote, timeout, cipher.encrypt)
+
+    def finish(self):
+        SocketServer.StreamRequestHandler.finish(self)
+
+
+def start_servers(config):
+    for serverinfo in config:
+        try:
+            logging.info('starting server: %s' % serverinfo)
+            ssserver = HXSocksServer(serverinfo, HXSocksHandler)
+            threading.Thread(target=ssserver.serve_forever).start()
+        except Exception as e:
+            logging.error('something wrong with config: %r' % e)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S', filemode='a+')
+
+    hello = 'hxsocks-server %s' % __version__
+    if gevent:
+        hello += ' with gevent %s' % gevent.__version__
+    print(hello)
+    print('by v3aqb')
+
+    ssserver = HXSocksServer('hxp://127.0.0.1:80', HXSocksHandler)
+    ssserver.serve_forever()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except socket.error as e:
+        logging.error(e)
+    except KeyboardInterrupt:
+        sys.exit(0)
